ecf28fa100b86357acf3a957c9842285a32a0923
Melissa Novais <melissa.novais@fatec.sp.gov.br>
2018-09-07 00:06:13 -0300

    toggle adicionado ao foormulario de cadastro

---
+++ js/home.js
@@ -0,0 +1,6 @@
+
+
+let btnSignup = document.getElementById("btn-signup");
+btnSignup.addEventListener("click", function () {
+    document.getElementById("form-signup").classList.toggle("show");
+})
